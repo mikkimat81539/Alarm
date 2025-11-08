267408d293eba3bb682728fc31d06436ee5a345e
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-08 00:02:49 -0600

    added JS style to days of the week

--- frontend/feature.js
+++ frontend/feature.js
@@ -1,10 +1,52 @@
-// When I click AM/PM color of container need to switch
-    // Everytime I click the switch container, the button colors for AM/PM need to change
-
+// Define Variables
 const btnSwitch = document.getElementById("switch")
+const amBtn = document.getElementById("AMbtn")
+const amBorder = document.getElementById("am")
 
-btnSwitch.addEventListener("click", colorToggle) 
+const pmBtn = document.getElementById("PMbtn")
+const pmBorder = document.getElementById("pm")
 
-function colorToggle() {
-    alert("Clicked")
+const dayBTN = document.querySelectorAll('.btnDay')
+
+// Event Listeners
+btnSwitch.addEventListener("click", colorBGToggleAM) 
+btnSwitch.addEventListener("click", colorBGTogglePM)
+
+// Loop
+dayBTN.forEach(button => {
+  button.addEventListener('click', colorDayToggle);
+});
+
+
+// Functions
+function colorBGToggleAM() {
+    if (amBtn.style.backgroundColor === "orange") {
+        amBtn.style.backgroundColor = "gray"
+        amBorder.style.border = "2px solid gray"
+    }
+
+    else {
+        amBtn.style.backgroundColor = "orange"
+        amBorder.style.border = "2px solid orange"
+    }
+}
+
+function colorBGTogglePM() {
+    if (pmBtn.style.backgroundColor === "gray") {
+        pmBtn.style.backgroundColor = "orange"
+        pmBorder.style.border = "2px solid orange"
+    }
+
+    else {
+        pmBtn.style.backgroundColor = "gray"
+        pmBorder.style.border = "2px solid gray"
+    }
+}
+
+function colorDayToggle() {
+  if (this.style.backgroundColor === 'orange') {
+    this.style.backgroundColor = 'white';
+  } else {
+    this.style.backgroundColor = 'orange';
+  }
 }
