7d576cdb3ed2b9d5e6856047005f2d43c76271ad
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-07 18:52:55 -0600

    first commit

---
+++ frontend/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 350
+  })
+
+  win.loadFile('frontend/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
